fcc6b5b3e4b0a71ab1e8c25d6b979a0b29dcaf0f
JahnaviYerram <97887220+jahnaviy26@users.noreply.github.com>
2024-05-30 14:29:38 +0000

    feat: avg endpoint

--- average-cal-express/controllers/numbers.controller.js
+++ average-cal-express/controllers/numbers.controller.js
@@ -1,10 +1,8 @@
-import { getNumber } from "../utils/utils.js";
+import { calculateAverage, getNumber } from "../utils/utils.js";
 
-
-const get_array = async (key) =>{
-    const array = await getNumber(key);
-}
-
+const get_array = async (key) => {
+  const array = await getNumber(key);
+};
 
 export const NumberHandler = async (req, res) => {
   const number_id = req.params.numberid;
@@ -12,11 +10,40 @@ export const NumberHandler = async (req, res) => {
   if (!number_id) {
     res.send({ msg: "please give the number id" });
   }
-  
-  let array ;
+
+  let array;
   switch (number_id) {
     case "p" | "f" | "e" | "r":
-        array = await get_array(key,array)
+      array = await get_array(key, array);
+      break;
+    default:
+      res.send({ msg: "not vaild number id " });
+  }
+
+  // switch (number_id) {
+  //   case "p" | "f" | "e" | "r":
+  //     break;
+
+  // }
+
+  switch (number_id) {
+    case "p":
+      array = await get_array(key, array);
+      res.send({
+        windowPrevState: [],
+        windowCurrState: array,
+        numbers: array,
+        avg: calculateAverage(array),
+      });
+      break;
+    case "f":
+      array = await get_array(key, array);
+      break;
+    case "e":
+      array = await get_array(key, array);
+      break;
+    case "r":
+      array = await get_array(key, array);
       break;
     default:
       res.send({ msg: "not vaild number id " });
